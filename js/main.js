f19eced0926cfca688dd73f0009dfde35f4b4f47
Erika Jenkins <erikamjenkins@ufl.edu>
2019-10-03 11:21:01 -0400

    removed comments with test code

--- js/main.js
+++ js/main.js
@@ -13,30 +13,25 @@ var myStartPlaceHolder = document.getElementById('startover');
 
 //this creates a button using javascript
 var myStartButton = document.createElement('button');
+
 //this creates what your button will output
 var myStartButtonText = document.createTextNode('Start Over');
+
 myStartButton.appendChild(myStartButtonText);
 
 
 
-//<div id= "startover"></div>
-//var myColorAll = document.querySelector('.myColorAll');
-//  <button id = "startover">Startover</button>
-
 function toggleRedQuote() {
-  //console.log(this);
   myRedMessage.classList.toggle('show-myRedMessage');
   myStartPlaceHolder.appendChild(myStartButton);
 };
 
 function toggleBlueQuote() {
-  //console.log(this);
   myBlueMessage.classList.toggle('show-myBlueMessage');
   myStartPlaceHolder.appendChild(myStartButton);
 };
 
 function toggleGreenQuote() {
-  //console.log(this);
   myGreenMessage.classList.toggle('show-myGreenMessage');
   myStartPlaceHolder.appendChild(myStartButton);
 };
